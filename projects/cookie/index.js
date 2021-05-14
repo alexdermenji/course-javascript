6270bdb843fcbfa76d227f4f4df99d14f5e62dd7
alexdermenji <allexdermenji@gmail.com>
2021-05-14 15:57:29 +0100

    comments

--- projects/cookie/index.js
+++ projects/cookie/index.js
@@ -45,6 +45,17 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
+//2. Если куки есть тогда очищаем талицу и создаем под каждый кук ячейку
+function printCookies(obj) {
+  if (getCookies()) {
+    listTable.innerHTML = '';
+    for (const [key, value] of Object.entries(obj)) {
+      createCell(key, value);
+    }
+  }
+}
+
+//3. Находим все куки  и превращаем их в обьект
 function getCookies() {
   let cookies;
   if (document.cookie) {
@@ -57,25 +68,7 @@ function getCookies() {
   return cookies;
 }
 
-function printCookies(obj) {
-  if (getCookies()) {
-    listTable.innerHTML = '';
-    for (const [key, value] of Object.entries(obj)) {
-      createCell(key, value);
-    }
-  }
-}
-
-function removeCookie(value) {
-  for (const child of listTable.childNodes) {
-    if (child.tagName === 'TR') {
-      if (child.firstElementChild.innerHTML === value) {
-        child.remove();
-      }
-    }
-  }
-}
-
+//4. Создаем ячейки в таблице
 function createCell(name, value) {
   const newCell = document.createElement('tr');
   listTable.append(newCell);
@@ -97,41 +90,67 @@ function createCell(name, value) {
   }
 }
 
-function sortCookies(key, value, symbol) {
+//7 Удаление куки из таблицы
+function removeCookie(value) {
+  for (const child of listTable.childNodes) {
+    if (child.tagName === 'TR') {
+      if (child.firstElementChild.innerHTML === value) {
+        child.remove();
+      }
+    }
+  }
+}
+
+//8 Проверяем если введенный текст в фильтр присутствует в таблице куков
+function filterCookies(key, value, symbol) {
   const keyValue = key + value;
   if (keyValue.includes(symbol)) {
     return true;
   } else return false;
 }
 
+//9 при вводе фильтра выводим новую таблицу с совпадениями куков
 filterNameInput.addEventListener('input', function (e) {
-  const value = e.target.value;
-  const obj = getCookies();
-  const sortObj = {};
+  const value = e.target.value; //значение из поля
+  const obj = getCookies(); // куки из браузера
+  const sortObj = {}; // сюда будем складывать новый отфильтрованный обьект
   for (const name in obj) {
-    if (sortCookies(name, obj[name], value)) {
-      sortObj[name] = obj[name];
+    // проходимся по всем кукам
+    if (filterCookies(name, obj[name], value)) {
+      //если значение из поля есть в куках
+      sortObj[name] = obj[name]; //создаем новые свойства в обьекте
     }
   }
-  return printCookies(sortObj);
+  return printCookies(sortObj); // возращаем новый вывод таблицы
 });
 
+//6 Обрабочик на создание куки
 addButton.addEventListener('click', () => {
+  //если куки есть удаляем из таблицы
   if (document.cookie.indexOf(addNameInput.value) >= 0) {
     removeCookie(addNameInput.value);
   }
+
+  //сохраняем в браузере
   document.cookie = `${addNameInput.value}=${addValueInput.value}`;
 
-  if (sortCookies(addNameInput.value, addValueInput.value, filterNameInput.value)) {
+  // если кука соответствует фильтру то создаем под нее ячейку
+  if (filterCookies(addNameInput.value, addValueInput.value, filterNameInput.value)) {
     createCell(addNameInput.value, addValueInput.value);
   }
+
+  //очищаем поля
   addNameInput.value = '';
   addValueInput.value = '';
 });
 
+//5 Устанавливаем обработчик клика на кнопку delete
 listTable.addEventListener('click', (e) => {
+  //удаляем из таблицы
   if (e.target.classList.contains('btn')) {
     e.target.parentElement.parentElement.remove();
+
+    //удаляем из браузера(????)
     const currentCookieName =
       e.target.parentElement.parentElement.firstElementChild.innerHTML;
     const currentCookieValue =
@@ -142,4 +161,22 @@ listTable.addEventListener('click', (e) => {
   }
 });
 
+//1. Выводим на экран все имеющиеся куки. В аргументе обьект
 printCookies(getCookies());
+
+/* 
+Вопросы
+1. в printCookies() Что лучше передать в аргумент, функцию или переменную?
+
+2. в createCell() Есть ои более компактный способ создать ячейки в таблице. И Не хардкодить цифры в цикле
+
+3. в listTable.addEventListener как можно избежать дублирования e.target.parentElement.parentElement.remove(); чтобы удалить всю строку
+
+4. Есть ли способ по другому удалить куки из браузера
+
+5.  в addButton.addEventListener можно ли не удалять куки из таблицы а просто изменять?
+
+6. function removeCookie() Можно по другому удалять куки из таблицы?
+
+7. function filterCookies() Можно по другому фильтровать?
+*/
